30306def9d8d65140e8a519eefe034adf30dfa08
VtSilveira <vitor.silveiravtss@gmail.com>
2025-06-18 14:39:08 -0300

    feat: ensure SignUpController returns 400 if no name is provided

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,5 +5,13 @@ export default [
     ...love,
     files: ["**/*.js", "**/*.ts"],
     ignores: ["dist", "node_modules"],
+    rules: {
+      ...love.rules,
+      "@typescript-eslint/no-magic-numbers": "off",
+      "@typescript-eslint/no-unsafe-assignment": "warn",
+      "@typescript-eslint/no-unsafe-member-access": "warn",
+      "@typescript-eslint/class-methods-use-this": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
   },
 ];
