def4fb1aaa6d39f7cc3e59bdda8a990b7074d2c2
VtSilveira <vitor.silveiravtss@gmail.com>
2025-07-02 15:55:17 -0300

    test: ensure SingUpController.handle returns 200 if everything is ok

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -138,22 +138,23 @@ describe("SignUp Controller", () => {
     expect(httpResponse.body).toEqual(new InvalidParamError("email"));
   });
 
-  test("Should call EmailValidator with correct email", () => {
-    const { sut, emailValidatorStub } = makeSut();
-
-    const isValidSpy = jest.spyOn(emailValidatorStub, "isValid");
+  test("Should return 400 if password and password confirmation are not the same", () => {
+    const { sut } = makeSut();
 
     const httpRequest = {
       body: {
         name: "any_name",
         email: "any_email@mail.com",
         password: "any_password",
-        passwordConfirmation: "any_password",
+        passwordConfirmation: "another_password",
       },
     };
 
-    sut.handle(httpRequest);
-    expect(isValidSpy).toHaveBeenCalledWith(httpRequest.body.email);
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(
+      new InvalidParamError("passwordConfirmation")
+    );
   });
 
   test("Should return 500 if EmailValidator throws an error", () => {
@@ -177,25 +178,62 @@ describe("SignUp Controller", () => {
     expect(httpResponse.body).toEqual(new ServerError());
   });
 
-  test("Should return 400 if password and password confirmation are not the same", () => {
-    const { sut } = makeSut();
+  test("Should return 500 if AddAccount throws an error", () => {
+    const { sut, addAcountStub } = makeSut();
+
+    jest.spyOn(addAcountStub, "add").mockImplementationOnce(() => {
+      throw new Error();
+    });
 
     const httpRequest = {
       body: {
         name: "any_name",
         email: "any_email@mail.com",
         password: "any_password",
-        passwordConfirmation: "another_password",
+        passwordConfirmation: "any_password",
       },
     };
 
     const httpResponse = sut.handle(httpRequest);
-    expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(
-      new InvalidParamError("passwordConfirmation")
-    );
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body).toEqual(new ServerError());
   });
 
+  test("Should call EmailValidator with correct email", () => {
+    const { sut, emailValidatorStub } = makeSut();
+
+    const isValidSpy = jest.spyOn(emailValidatorStub, "isValid");
+
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email@mail.com",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+
+    sut.handle(httpRequest);
+    expect(isValidSpy).toHaveBeenCalledWith(httpRequest.body.email);
+  });
+
+  test("Should call EmailValidator with correct email", () => {
+    const { sut, emailValidatorStub } = makeSut();
+
+    const isValidSpy = jest.spyOn(emailValidatorStub, "isValid");
+
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email@mail.com",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+
+    sut.handle(httpRequest);
+    expect(isValidSpy).toHaveBeenCalledWith(httpRequest.body.email);
+  });
   test("Should call AddAcount with correct values", () => {
     const { sut, addAcountStub } = makeSut();
 
@@ -218,24 +256,25 @@ describe("SignUp Controller", () => {
     });
   });
 
-  test("Should return 500 if AddAccount throws an error", () => {
-    const { sut, addAcountStub } = makeSut();
-
-    jest.spyOn(addAcountStub, "add").mockImplementationOnce(() => {
-      throw new Error();
-    });
+  test("Should return 200 if valid data is provided", () => {
+    const { sut } = makeSut();
 
     const httpRequest = {
       body: {
-        name: "any_name",
-        email: "any_email@mail.com",
-        password: "any_password",
-        passwordConfirmation: "any_password",
+        name: "valid _name",
+        email: "valid_mail@mail.com",
+        password: "valid_password",
+        passwordConfirmation: "valid_password",
       },
     };
 
     const httpResponse = sut.handle(httpRequest);
-    expect(httpResponse.statusCode).toBe(500);
-    expect(httpResponse.body).toEqual(new ServerError());
+    expect(httpResponse.statusCode).toBe(200);
+    expect(httpResponse.body).toEqual({
+      id: "valid_id",
+      name: "valid_name",
+      email: "valid_mail@mail.com",
+      password: "valid_password",
+    });
   });
 });
